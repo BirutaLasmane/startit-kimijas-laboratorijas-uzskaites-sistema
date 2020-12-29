eba813d067a82c807f50600b97afa6c777ac4ae7
Biruta Lasmane <biruta.lasmane@jvg.edu.lv>
2020-12-29 09:20:43 +0000

    Izveidots route sveiki

--- serveris.py
+++ serveris.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Labrīt!"
-
+    
+@app.route('/sveiki')
+def sveiki():
+  return "Nav vairs nekāds rīts!"
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
